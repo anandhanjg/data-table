2dca803cef5e16f400c335909837190c5778ea62
anandhanjg <jganandhan@d-devlabs.com>
2020-08-27 19:27:52 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component,OnInit } from '@angular/core';
-import {TableService} from './table.service';
+import { Component } from '@angular/core';
+import { TableService } from './table.service';
 
 @Component({
   selector: 'app-root',
